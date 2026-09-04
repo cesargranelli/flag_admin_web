4da25783ed12d19f5c02990f71db2faea62afe64
Developer <developer@example.com>
2026-09-04 09:24:15 -0300

    fix: corrigir imports em athlete_detail_screen (caminho relativo)

--- lib/src/features/athletes/presentation/screens/athlete_detail_screen.dart
+++ lib/src/features/athletes/presentation/screens/athlete_detail_screen.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-import '../providers/providers.dart';
-import '../utils/date_formats.dart';
-import '../widgets/app_screen.dart';
+import '../../../../providers/providers.dart';
+import '../../../../utils/date_formats.dart';
+import '../../../../core/widgets/app_screen.dart';
 
 /// Detalhe de um atleta: apresenta os dados e oferece a edição.
 ///
